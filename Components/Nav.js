8b797f4d8404ccc3d896a56c937d8eaf483e296c
nitish2101 <nitishsharma50@gmail.com>
2022-12-19 04:34:23 +0530

    layout

--- Components/Nav.js
+++ Components/Nav.js
@@ -1,7 +1,44 @@
+/* eslint-disable @next/next/no-img-element */
+import Link from "next/link";
+import { auth } from "../utils/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import Image from "next/image";
+
 export default function Nav() {
+  const [user, loading] = useAuthState(auth);
+  console.log(user);
+
   return (
-    <div className='mx-6 md:max-w-2xl md:mx-auto font-poppins'>
-      <h1>Nav</h1>
-    </div>
+    <nav className='flex justify-between items-centre py-10'>
+      <Link href='/'>
+        <button className='text-lg font-medium'>Creative Minds</button>
+      </Link>
+      <ul className='flex items-centre gap-10'>
+        {!user && (
+          <Link
+            className='py-2 px-4 text-sm bg-cyan-500 text-white rounded-lg font-medium ml-8'
+            href={"/auth/login"}
+          >
+            Join Now
+          </Link>
+        )}
+        {user && (
+          <div className='flex item-centre gap-6'>
+            <Link href='/post'>
+              <button className='font-medium bg-cyan-500 text-white py-2 px-4 roounded-mg textx-sm'>
+                Post
+              </button>
+            </Link>
+            <Link href='/dashboard'>
+              <img
+                className='w-10 rounded-full cursor-pointer'
+                src={user.photoURL}
+                alt='user photo'
+              />
+            </Link>
+          </div>
+        )}
+      </ul>
+    </nav>
   );
 }
