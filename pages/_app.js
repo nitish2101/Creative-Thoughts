ded9aa120cf2535cb421c439f9c07d2eb66bbeb8
nitish2101 <nitishsharma50@gmail.com>
2022-12-21 01:21:06 +0530

    dashboard created

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,12 @@
 import "../styles/globals.css";
+import { ToastContainer } from "react-toastify";
 import Layout from "../Components/layout";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
+      <ToastContainer limit={1} />
       <Component {...pageProps} />
     </Layout>
   );
---
+++ pages/dashboard.js
@@ -0,0 +1,25 @@
+import { auth } from "../utils/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+export default function Dashboard() {
+  const route = useRouter();
+  const [user, loading] = useAuthState(auth);
+
+  const getData = async () => {
+    if (loading) return;
+    if (!user) return route.push("/auth/login");
+  };
+  useEffect(() => {
+    getData();
+  }, [user, loading]);
+
+  return (
+    <div>
+      <h1>Your Posts</h1>
+      <div>posts</div>
+      <button onClick={() => auth.signOut()}>Sign Out</button>
+    </div>
+  );
+}
